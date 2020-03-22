a25c4510275c542c8a0b571d442b31336385ffa0
Shucheng Yang <ysc@shao.ac.cn>
2020-03-22 23:36:51 +0800

    clean up

--- fpfc2w.py
+++ fpfc2w.py
@@ -134,30 +134,102 @@ def WaveDataOut(**kwargs):
 #     waveData.to_csv('waveDataEMRI.dat')# columns=['JDTimeTCB']  #导出csv文件
     return waveData
 
+#######探测器响应，依赖于探测器轨道数据#######################
+def orbitDataProcess(orbitDataTAIJI, NVec):
+    orbitData      =  orbitDataTAIJI.loc[0:366,:]
+    sunPX = orbitData['sunPX']
+    sunPY = orbitData['sunPY']
+    sunPZ = orbitData['sunPZ']
 
-# In[ ]:
+    earthPX = orbitData['earthPX']
+    earthPY = orbitData['earthPY']
+    earthPZ = orbitData['earthPZ']
+
+    scp1X = orbitData['scp1X']
+    scp1Y = orbitData['scp1Y']
+    scp1Z = orbitData['scp1Z']
+
+    scp2X = orbitData['scp2X']
+    scp2Y = orbitData['scp2Y']
+    scp2Z = orbitData['scp2Z']
+
+    scp3X = orbitData['scp3X']
+    scp3Y = orbitData['scp3Y']
+    scp3Z = orbitData['scp3Z']
+
+    scpX = (scp1X + scp2X + scp3X) / 3  
+    scpY = (scp1Y + scp2Y + scp3Y) / 3  
+    scpZ = (scp1Z + scp2Z + scp3Z) / 3  
+
+    scp = (np.vstack([scpX, scpY, scpZ])).T
+    scp1 = (np.vstack([scp1X, scp1Y, scp1Z])).T
+    scp2 = (np.vstack([scp2X, scp2Y, scp2Z])).T
+    scp3 = (np.vstack([scp3X, scp3Y, scp3Z])).T
+
+    d12 = scp2 - scp1 
+    d13 = scp3 - scp1
+    xd  = d12 + d13
+
+    ed12 = d12 / np.linalg.norm(d12, axis=1, keepdims=True)
+    ed13 = d13 / np.linalg.norm(d13, axis=1, keepdims=True)
+
+    exd = xd / np.linalg.norm(xd, axis=1, keepdims=True)
+    ezd = vecCross(ed13, ed12)
+    eyd = vecCross(ezd, exd)
 
 
+    trans = transMat(exd, eyd, ezd)
+
+
+    LVec = NVec + np.array([[1e-13,1e-13,1e-13]])#randUnitVec(1)
+
+    LVec = LVec / np.linalg.norm(LVec, axis=1, keepdims=True)
+
+    thetaPhi = W2ThetaPhi(trans, NVec)
+    theta = thetaPhi[:,0]
+    phi = thetaPhi[:,1]
+    psi = LNZ2Psi(LVec, NVec, ezd)[:,0]
+    thetaPhiPsi = np.array([theta, phi, psi])
+    
+    ####
+    timeVec = orbitData['timeJD']
+    Fp1 = Fplus1(theta, phi, psi)
+    Fc1 =  Fcross1(theta, phi, psi)
+    Fp2 = Fplus2(theta, phi, psi)
+    Fc2 =  Fcross2(theta, phi, psi)
+    ans = np.vstack([timeVec, Fp1, Fc1, Fp2, Fc2])
+    
+    return ans, thetaPhiPsi
 
 if __name__ == '__main__':
-    
 
-    MassBH    = 1e6
-    SpinxBH   = 0
-    SpinyBH   = 0
-    SpinzBH   = 0.9
-    MassCO    = 1e1
-    SpinxCO   = 0 
-    SpinyCO   = 0
-    SpinzCO   = 90
-    ECC       = 0.5
-    PM        = 20
-    IOTA      = 0.25
+    #####源位置###############
+    NVec = np.array([[-0.21754308, -0.88794532, -0.40525068]]) #randUnitVec(1)
 
+    #####其他参数#############
+    # MassBH    = 1e6       
+    # SpinxBH   = 0
+    # SpinyBH   = 0
+    # SpinzBH   = 0.9
+    # MassCO    = 1e1
+    # SpinxCO   = 0 
+    # SpinyCO   = 0
+    # SpinzCO   = 90
+    # ECC       = 0.5
+    # PM        = 20
+    # IOTA      = 0.25
 
-    kwargs = dict(MassBH=MassBH, SpinxBH=SpinxBH, SpinyBH=SpinyBH, SpinzBH=SpinzBH,     MassCO=MassCO, SpinxCO=SpinxCO, SpinyCO=SpinyCO, SpinzCO=SpinzCO, ECC=ECC, PM=PM, IOTA=IOTA)
+    # kwargs = dict(MassBH=MassBH, SpinxBH=SpinxBH, SpinyBH=SpinyBH, SpinzBH=SpinzBH, \
+    #     MassCO=MassCO, SpinxCO=SpinxCO, SpinyCO=SpinyCO, SpinzCO=SpinzCO, ECC=ECC, PM=PM, IOTA=IOTA)
 
-    wave = WaveDataOut(**kwargs)
-    wave.to_csv('waveDataEMRIyear.dat')# columns=['JDTimeTCB']  #导出csv文件
+    # wave = WaveDataOut(**kwargs)
+
+    wave =  pd.read_csv('./others/waveDataEMRImonth.dat', index_col = 0)  #读取csv文件
+
+    ##########太极轨道数据##############################################################################################
+    orbitDataTAIJI  =  pd.read_csv('orbitData/orbitDataTAIJI.dat', index_col = 0)  #读取csv文件
+
+    ans, thetaPhiPsi = orbitDataProcess(orbitDataTAIJI, NVec) #响应函数 pattern function 输出 timeVec, Fp1, Fc1, Fp2, Fc2
+                                                              #以及源的theta, phi, psi
 
 
